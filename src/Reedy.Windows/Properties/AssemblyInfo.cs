580add34e595f40ee11ea5a273665e6f50f6a4bd
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2016-12-04 01:57:11 -0500

    Reedy: Move some stuff around.

--- src/Reedy.Windows/Properties/AssemblyInfo.cs
+++ src/Reedy.Windows/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Reedy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Reedy is an iPhone backup reader & extractor.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Reedy")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Bojan Rajkovic 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
